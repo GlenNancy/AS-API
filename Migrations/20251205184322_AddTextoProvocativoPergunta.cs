385d3d6dbb385cc306406f44dc738444f4d74c64
glennancy <glennancy24@gmail.com>
2025-12-05 15:51:36 -0300

    Nova migration X

---
+++ Migrations/20251205184322_AddTextoProvocativoPergunta.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace As.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTextoProvocativoPergunta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TextoProvocativo",
+                table: "Perguntas",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TextoProvocativo",
+                table: "Perguntas");
+        }
+    }
+}
